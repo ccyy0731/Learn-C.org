4eec8daae4bd93b9dcd9d8836b0b3e771e0e9d06
ccyy0731 <67245934+ccyy0731@users.noreply.github.com>
2020-06-21 20:09:40 -0400

    Create Arrays.c

---
+++ Arrays.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+int main() {
+  /* TODO: define the grades variable here */
+  int grades[2];
+    
+  int average;
+
+  grades[0] = 80;
+  /* TODO: define the missing grade
+     so that the average will sum to 85. */
+  grades[1] = 85;
+  grades[2] = 90;
+
+  average = (grades[0] + grades[1] + grades[2]) / 3;
+  printf("The average of the 3 grades is: %d", average);
+
+  return 0;
+}
